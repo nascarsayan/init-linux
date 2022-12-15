8ef8315823f61e19a2ad6234a59d0e1695a5482b
Sayan Naskar <nascarsayan@gmail.com>
2022-12-15 19:49:05 +0530

    tmux on login

--- home/.oh-my-zsh/custom/01_shell_util.zsh
+++ home/.oh-my-zsh/custom/01_shell_util.zsh
@@ -9,3 +9,12 @@ eval "$(fasd --init auto)"
 
 # disable bell and beep
 unsetopt beep
+
+# tmux on login
+tmux_on_login() {
+  if [[ -n "$PS1" ]] && [[ -z "$TMUX" ]] && [[ -n "$SSH_CONNECTION" ]] && [[ ! $(command -v code 2>/dev/null) ]]; then
+    tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
+  fi
+}
+
+tmux_on_login
