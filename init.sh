ce3f4b1ea863328ec07a81035df5dd3064c3ba03
Sayan Naskar <nascarsayan@gmail.com>
2025-08-16 12:16:14 +0530

    Migrate to zinit

--- init.sh
+++ init.sh
@@ -2,8 +2,11 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# Install base packages and tools
 install_packages() {
+  echo "Installing base packages..."
   brew_packages=(nnn vim git zsh tmux tree jq htop curl wget nano iputils bison bat neovim tmux)
+  
   if [[ "$OSTYPE" == "darwin"* ]]; then
     if ! command -v brew &> /dev/null; then
       echo "Homebrew is not installed. Please install Homebrew first."
@@ -19,13 +22,14 @@ install_packages() {
       sudo -E apt update
       sudo -E apt install -y nnn build-essential net-tools vim git zsh tmux fasd tree jq htop curl wget nano iputils-ping mercurial bison bat
 
+      # Install neovim
       curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
       chmod u+x nvim.appimage
       ./nvim.appimage --appimage-extract > /dev/null 2>&1
       sudo mv squashfs-root /
       sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
     else
-      echo "Neither Homebrew nor apt is installed. Please install one of them first."
+      echo "Neither Homebrew nor apt is installed. Please install one of them first to install the initial packages"
       exit 1
     fi
   else
@@ -34,106 +38,112 @@ install_packages() {
   fi
 }
 
-install_packages
+# Install additional tools (yq, node)
+install_tools() {
+  echo "Installing additional tools..."
+  
+  # Install yq
+  sudo curl -L https://github.com/mikefarah/yq/releases/download/v4.13.3/yq_linux_amd64 -o /usr/bin/yq && sudo chmod +x /usr/bin/yq
+  
+  # Install node via n-install
+  curl -L https://git.io/n-install | bash -s -- -y
+}
 
-sudo curl -L https://github.com/mikefarah/yq/releases/download/v4.13.3/yq_linux_amd64 -o /usr/bin/yq && sudo chmod +x /usr/bin/yq
-curl -L https://git.io/n-install | bash -s -- -y
-
-# if ~/.oh-my-zsh exists, confirm if we should delete it. if no selected, exit
-if [ -d "$HOME/.oh-my-zsh" ]; then
-  # if folder is emoty delete it, else ask user if they want to delete it
-  if [ -z "$(ls -A "$HOME/.oh-my-zsh")" ]; then
-    echo "Deleting empty oh-my-zsh folder without confirmation."
-    rm -rf "$HOME/.oh-my-zsh"
-  else
-    read -p "oh-my-zsh already exists. Do you want to delete it? [y/N] " -r
-    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-      echo "Exiting script because oh-my-zsh already exists and user chose not to delete it."
-      return
-    fi
+# Configure zsh with zinit
+configure_zsh() {
+  echo "Configuring zsh with zinit..."
+  
+  # Clean up any existing oh-my-zsh installation
+  if [ -d "$HOME/.oh-my-zsh" ]; then
+    echo "Removing existing oh-my-zsh installation"
     rm -rf "$HOME/.oh-my-zsh"
   fi
-fi
 
-# * Zsh setup
-# install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-if [ ! -f "$HOME/.zshrc" ]; then
-  echo "oh-my-zsh could not be cloned. Please try again."
-  return
-fi
+  # Install Zinit
+  ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+  mkdir -p "$(dirname $ZINIT_HOME)"
+  git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 
-zsh_custom=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
-# install zsh autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions "$zsh_custom/plugins/zsh-autosuggestions"
+  # Create basic .zshrc if it doesn't exist
+  if [ ! -f "$HOME/.zshrc" ]; then
+    touch "$HOME/.zshrc"
+  fi
 
-# install zsh syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$zsh_custom/plugins/zsh-syntax-highlighting"
+  # Configure .zshrc for Zinit from template
+  if [ -f "$SCRIPT_DIR/templates/zshrc-tpl.zsh" ]; then
+    cp "$SCRIPT_DIR/templates/zshrc-tpl.zsh" "$HOME/.zshrc"
+  else
+    echo "Warning: zshrc-tpl.zsh template not found. Creating basic .zshrc..."
+    echo "# Basic zsh configuration - please add your custom settings" > "$HOME/.zshrc"
+  fi
 
-# install kubectl aliases
-curl -L https://raw.githubusercontent.com/ahmetb/kubectl-alias/master/.kubectl_aliases -o "$zsh_custom/20_kubectl.zsh"
+  echo "
+## Zsh configured with Zinit:
 
-# install p10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$zsh_custom/themes/powerlevel10k"
-
-# install spaceship prompt
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$zsh_custom/themes/spaceship-prompt" --depth=1
-ln -s "$zsh_custom/themes/spaceship-prompt/spaceship.zsh-theme" "$zsh_custom/themes/spaceship.zsh-theme"
-
-# github theme
-curl -L https://raw.githubusercontent.com/Debdut/github.zsh-theme/main/github.zsh-theme -o "$zsh_custom/themes/github.zsh-theme"
-
-perl -i -pe 's~robbyrussell~github~' "$HOME"/.zshrc
-perl -i -pe "s~#\s*(?=DISABLE_MAGIC_FUNCTIONS)~~" "$HOME"/.zshrc
-perl -i -pe "s~#\s*(?=DISABLE_UPDATE_PROMPT)~~" "$HOME"/.zshrc
-perl -i -pe "s~# (?=(zstyle ':omz:update' mode auto))~~" "$HOME"/.zshrc
-plugins="\n  git\n  zsh-syntax-highlighting\n  dircycle\n  urltools\n  zsh-autosuggestions\n  sudo\n  vscode\n  fasd\n  colored-man-pages\n  git-extras\n  kubectl\n  docker  \n  docker-compose helm\n"
-perl -i -pe "s~(?<=plugins=\()git(?=\))~$plugins~" "$HOME"/.zshrc
-perl -i -pe "s~^source~autoload -U +X bashcompinit && bashcompinit\n\nsource~" "$HOME"/.zshrc
-
-# install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
-
-echo "Configuring tmux"
-git clone https://github.com/gpakosz/.tmux.git ~/.tmux
-ln -s -f ~/.tmux/.tmux.conf ~/.tmux.conf
-git clone https://github.com/nascarsayan/.tmux.local.git ~/.tmux.local
-ln -s -f ~/.tmux.local/.tmux.conf.local ~/.tmux.conf.local
-
-# Tell the patches to make
-echo "
-## Have made the following changes:
-
-ZSH_THEME=\"spaceship\"
-
-plugins=(
-  git
-  zsh-syntax-highlighting
-  dircycle
-  urltools
-  zsh-autosuggestions
-  sudo
-  vscode
-  fasd
-  colored-man-pages
-  git-extras
-  kubectl
-  helm
-)
-
-autoload -U +X bashcompinit && bashcompinit
-
-DISABLE_MAGIC_FUNCTIONS=true
-DISABLE_UPDATE_PROMPT=true
+- Installed Zinit plugin manager
+- Using Powerlevel10k theme (run 'p10k configure' to customize)
+- Loaded plugins: git, sudo, colored-man-pages, kubectl, docker, helm, vscode, git-extras
+- Loaded zsh-users plugins: autosuggestions, syntax-highlighting, completions
+- Kubectl aliases available
+- Settings: DISABLE_MAGIC_FUNCTIONS=true, DISABLE_UPDATE_PROMPT=true
 "
+}
 
-# * Copy customizations
-chmod 0700 "$SCRIPT_DIR/home/.ssh"
-find "$SCRIPT_DIR/home/.ssh" -not -name "*.pub" -name "id_rsa*" -exec chmod 0600 "{}" "+"
+# Install and configure fzf
+install_fzf() {
+  echo "Installing fzf..."
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install --all
+}
 
-rsync -a "$SCRIPT_DIR/home/" ~
-perl -i -pe "s/uname/$USER/" ~/.gitconfig
+# Configure tmux
+configure_tmux() {
+  echo "Configuring tmux..."
+  git clone https://github.com/gpakosz/.tmux.git ~/.tmux
+  ln -s -f ~/.tmux/.tmux.conf ~/.tmux.conf
+  git clone https://github.com/nascarsayan/.tmux.local.git ~/.tmux.local
+  ln -s -f ~/.tmux.local/.tmux.conf.local ~/.tmux.conf.local
+}
 
-echo "Changing the default shell to zsh"
-sudo chsh -s "$(which zsh)" "$USER"
+# Copy user customizations
+copy_customizations() {
+  echo "Copying customizations..."
+  chmod 0700 "$SCRIPT_DIR/home/.ssh"
+  find "$SCRIPT_DIR/home/.ssh" -not -name "*.pub" -name "id_rsa*" -exec chmod 0600 "{}" "+"
+
+  rsync -a "$SCRIPT_DIR/home/" ~
+  perl -i -pe "s/uname/$USER/" ~/.gitconfig
+}
+
+# Change default shell to zsh
+change_shell() {
+  echo "Changing the default shell to zsh"
+  sudo chsh -s "$(which zsh)" "$USER"
+}
+
+# Main function that orchestrates all setup
+main() {
+  echo "Starting system initialization..."
+  
+  # install_packages
+  # install_tools
+  configure_zsh
+  install_fzf
+  # configure_tmux
+  # copy_customizations
+  change_shell
+  
+  echo "
+🎉 System initialization completed!
+
+Next steps:
+- Restart your shell or run 'exec zsh'
+- Run 'p10k configure' to customize your prompt
+- Your tmux and other configurations are ready to use
+"
+}
+
+# Run main function if script is executed directly
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  main "$@"
+fi
---
+++ templates/zshrc-tpl.zsh
@@ -0,0 +1,42 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# Zinit installation
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+source "${ZINIT_HOME}/zinit.zsh"
+
+# Load powerlevel10k theme
+zinit ice depth=1; zinit load romkatv/powerlevel10k
+
+# Oh My Zsh plugins
+zinit snippet OMZ::plugins/git/git.plugin.zsh
+zinit snippet OMZ::plugins/sudo/sudo.plugin.zsh
+zinit snippet OMZ::plugins/colored-man-pages/colored-man-pages.plugin.zsh
+zinit snippet OMZ::plugins/kubectl/kubectl.plugin.zsh
+zinit snippet OMZ::plugins/docker/docker.plugin.zsh
+zinit snippet OMZ::plugins/docker-compose/docker-compose.plugin.zsh
+zinit snippet OMZ::plugins/helm/helm.plugin.zsh
+zinit snippet OMZ::plugins/vscode/vscode.plugin.zsh
+zinit snippet OMZ::plugins/git-extras/git-extras.plugin.zsh
+
+# Load zsh-users plugins
+zinit load zsh-users/zsh-autosuggestions
+zinit load zsh-users/zsh-syntax-highlighting
+zinit load zsh-users/zsh-completions
+
+# Load kubectl aliases
+zinit snippet https://raw.githubusercontent.com/ahmetb/kubectl-alias/master/.kubectl_aliases
+
+# Settings
+DISABLE_MAGIC_FUNCTIONS=true
+DISABLE_UPDATE_PROMPT=true
+
+# Enable bash completions
+autoload -U +X bashcompinit && bashcompinit
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+command -v zoxide > /dev/null && eval "$(zoxide init zsh)"
